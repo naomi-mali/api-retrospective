c54599f9b757038aa636bd127f5614187195b189
Timea Mali <derzsi_timea@yahoo.com>
2024-10-23 13:37:30 +0000

    added profile serialzer and list view

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=75, blank=True)
+    name = models.CharField(max_length=250, blank=True)
     bio = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_mjfgzn'
