6af0106213afd772982d381ab9b4373512cde2a4
Timea Mali <derzsi_timea@yahoo.com>
2024-10-27 16:37:45 +0000

    add PostDetail test

--- posts/tests.py
+++ posts/tests.py
@@ -26,3 +26,49 @@ class PostListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    """Post detail view tests"""
+
+    def setUp(self):
+        self.adam = User.objects.create_user(username='adam', password='pass')
+        self.brian = User.objects.create_user(username='brian', password='pass')
+        self.post1 = Post.objects.create(
+            owner=self.adam, title='a title'
+        )
+        self.post2 = Post.objects.create(
+            owner=self.brian, title='another title'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get(f'/posts/{self.post1.id}/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put(f'/posts/{self.post1.id}/', {'title': 'a new title'})
+        self.post1.refresh_from_db()
+        self.assertEqual(self.post1.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put(f'/posts/{self.post2.id}/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_own_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.delete(f'/posts/{self.post1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Post.objects.filter(id=self.post1.id).exists())
+
+    def test_user_cant_delete_another_users_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.delete(f'/posts/{self.post2.id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
