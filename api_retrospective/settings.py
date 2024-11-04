69888ab095d43572478883f333179a4600eab8e6
Timea Mali <derzsi_timea@yahoo.com>
2024-11-04 15:53:17 +0000

    set debug to true

--- api_retrospective/settings.py
+++ api_retrospective/settings.py
@@ -61,8 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV'in os.environ
-
+DEBUG = True
 ALLOWED_HOSTS = [
     '8000-naomimali-apiretrospect-oizhpjirc0y.ws.codeinstitute-ide.net',
     os.environ.get('ALLOWED_HOST'),
